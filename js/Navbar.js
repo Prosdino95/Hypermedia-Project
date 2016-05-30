dfd02d9dddfae94a923cb1fdf4b1f387a045830d
Marco Prosdocimi <ProsdoTheWolf@MacBook-Pro-di-Marco.local>
2016-05-30 20:21:59 +0200

    Navbar
    aggiunte animazioni menù

---
+++ js/Navbar.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+        $('.dropdown').hover(
+        function() {
+            $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+        }, 
+        function() {
+            $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+        }
+    );
+
+    $('.dropdown-menu').hover(
+        function() {
+            $(this).stop(true, true);
+        },
+        function() {
+            $(this).stop(true, true).delay(200).fadeOut();
+        }
+    );
+});
+
+
+
